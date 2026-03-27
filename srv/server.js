a08492ffe462d469e6cad3ec2c70d6566315867b
sama jaikaran reddy <samajaikaranreddy@gmail.com>
2026-03-27 10:13:00 +0000

    Fixed deployment issues: node version, start command, MTA config

--- srv/server.js
+++ srv/server.js
@@ -4,4 +4,4 @@ cds.on('bootstrap', app => {
   app.get('/', (req, res) => res.send('Sales Order App Running '))
 })
 
-module.exports = cds.server
+module.exports = cds.servercf logs sales-order-app-srv --recent
